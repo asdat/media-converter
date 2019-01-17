c261503f408318feb7444dc4ea7c39122cb03377
Aleksandr Datsko <info@asdat.org>
2019-01-17 21:19:31 +0200

    Test

--- app/Jobs/EncodingMediaJob.php
+++ app/Jobs/EncodingMediaJob.php
@@ -106,6 +106,7 @@ class EncodingMediaJob extends Job implements ShouldQueue
 
         $sendRequestFlag = true;
         foreach ($outputExtGroup as $extension) {
+            Log::info($outputFileWithoutExtension . '.' . $extension . ' + ' . $sendRequestFlag);
             if (!File::exists($outputFileWithoutExtension . '.' . $extension)) {
                 $sendRequestFlag = false;
                 break;
