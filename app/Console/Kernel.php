38aec16044a9b69918112b3b6e42248cce182fa6
Aleksandr Datsko <info@asdat.org>
2019-02-18 14:03:13 +0200

    MC-8 Compare MQS for performance
    - registered new console command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\PutMediaIntoQueue::class,
+        Commands\PutTestTaskIntoQueue::class
     ];
 
     /**
